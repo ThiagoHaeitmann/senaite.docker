3914db9f547491225a59d0f4fa39a28c8c5705f6
Thiago Haeitmann <135155756+ThiagoHaeitmann@users.noreply.github.com>
2025-12-30 08:39:32 -0300

    Update docker-initialize.py

--- latest/docker-initialize.py
+++ latest/docker-initialize.py
@@ -1,41 +1,40 @@
 #!/usr/local/bin/python
-
-import re
 import os
+import re
 
 
 class Environment(object):
-    """ Configure container via environment variables
-    """
+    """Configure container via environment variables"""
+
     def __init__(
         self, env=os.environ,
         zope_conf="/home/senaite/senaitelims/parts/instance/etc/zope.conf",
         custom_conf="/home/senaite/senaitelims/custom.cfg",
         zeopack_conf="/home/senaite/senaitelims/bin/zeopack",
-        zeoserver_conf="/home/senaite/senaitelims/parts/zeo/etc/zeo.conf",
         cors_conf="/home/senaite/senaitelims/parts/instance/etc/package-includes/999-additional-overrides.zcml"
     ):
         self.env = env
         self.zope_conf = zope_conf
         self.custom_conf = custom_conf
         self.zeopack_conf = zeopack_conf
-        self.zeoserver_conf = zeoserver_conf
         self.cors_conf = cors_conf
-    if not os.path.exists(self.zeoserver_conf):
-        self.zeoserver_conf = "/home/senaite/senaitelims/parts/zeoserver/etc/zeo.conf"
+
+        # ZEO server config: depending on buildout it can be in parts/zeo or parts/zeoserver
+        self.zeo_confs = [
+            "/home/senaite/senaitelims/parts/zeo/etc/zeo.conf",
+            "/home/senaite/senaitelims/parts/zeoserver/etc/zeo.conf",
+        ]
 
     def zeoclient(self):
-        """ ZEO Client
-        """
+        """ZEO Client"""
         server = self.env.get("ZEO_ADDRESS", None)
         if not server:
             return
 
-        config = ""
         with open(self.zope_conf, "r") as cfile:
             config = cfile.read()
 
-        # Already initialized
+        # Already initialized (no blobstorage marker)
         if "<blobstorage>" not in config:
             return
 
@@ -44,6 +43,7 @@ class Environment(object):
         shared_blob_dir = self.env.get("ZEO_SHARED_BLOB_DIR", "off")
         zeo_storage = self.env.get("ZEO_STORAGE", "1")
         zeo_client_cache_size = self.env.get("ZEO_CLIENT_CACHE_SIZE", "128MB")
+
         zeo_conf = ZEO_TEMPLATE.format(
             zeo_address=server,
             read_only=read_only,
@@ -58,85 +58,80 @@ class Environment(object):
 
         with open(self.zope_conf, "w") as cfile:
             cfile.write(config)
-    
 
-    def set_zeo_port(self):
-        """Set zeo server port in parts/zeo/etc/zeo.conf"""
+    def set_zeo_bind_port(self):
+        """Set zeo server bind/port in zeo.conf (server side)"""
         zeo_port = self.env.get("ZEO_PORT", "").strip()
+        zeo_bind = self.env.get("ZEO_BIND", "").strip() or "127.0.0.1"
         if not zeo_port:
             return
-    
-        if not os.path.exists(self.zeoserver_conf):
-            return
-    
-        with open(self.zeoserver_conf, "r") as f:
-            text = f.read()
-    
-        text_new = text
-    
-        # cobre:
-        # address 8080
-        # address 127.0.0.1:8080
-        # address 0.0.0.0:8080
-        # address [::]:8080
-        text_new = re.sub(r'(^\s*address\s+)\d+\s*$',
-                          r'\g<1>%s' % zeo_port, text_new, flags=re.M)
-        text_new = re.sub(r'(^\s*address\s+127\.0\.0\.1:)\d+\s*$',
-                          r'\g<1>%s' % zeo_port, text_new, flags=re.M)
-        text_new = re.sub(r'(^\s*address\s+0\.0\.0\.0:)\d+\s*$',
-                          r'\g<1>%s' % zeo_port, text_new, flags=re.M)
-        text_new = re.sub(r'(^\s*address\s+\[::\]:)\d+\s*$',
-                          r'\g<1>%s' % zeo_port, text_new, flags=re.M)
-    
-        if text_new != text:
-            with open(self.zeoserver_conf, "w") as f:
-                f.write(text_new)
 
-    
-    
+        for conf in self.zeo_confs:
+            if not os.path.exists(conf):
+                continue
+
+            with open(conf, "r") as f:
+                text = f.read()
+
+            new = text
+
+            # Replace any address form with bind:port
+            # address 8080
+            # address 127.0.0.1:8080
+            # address 0.0.0.0:8080
+            # address [::]:8080
+            new = re.sub(
+                r'(^\s*address\s+)(?:([0-9a-fA-F\.\:\[\]]+):)?(\d+)\s*$',
+                lambda m: m.group(1) + ("%s:%s" % (zeo_bind, zeo_port)),
+                new, flags=re.M
+            )
+
+            # If no address line exists at all, append one (safe)
+            if not re.search(r'^\s*address\s+', new, flags=re.M):
+                new += "\n  address %s:%s\n" % (zeo_bind, zeo_port)
+
+            if new != text:
+                with open(conf, "w") as f:
+                    f.write(new)
+
     def set_http_port(self):
         """Set instance HTTP port (zope.conf)"""
         http_port = self.env.get("HTTP_PORT", "").strip()
         if not http_port:
             return
-    
+
         if not os.path.exists(self.zope_conf):
             return
-    
+
         with open(self.zope_conf, "r") as f:
             text = f.read()
-    
-        text_new = text
-    
-        # pega:
+
+        new = text
+
+        # Matches:
         # http-address 8080
         # http-address = 8080
         # http-address 0.0.0.0:8080
         # http-address = 0.0.0.0:8080
-        text_new = re.sub(
+        new = re.sub(
             r'(^\s*http-address\s*=?\s*)(?:([0-9a-fA-F\.\:\[\]]+):)?(\d+)\s*$',
             lambda m: m.group(1) + ((m.group(2) + ":") if m.group(2) else "") + http_port,
-            text_new,
-            flags=re.M
+            new, flags=re.M
         )
-    
-        # alguns templates usam "address" em vez de "http-address"
-        text_new = re.sub(
+
+        # Some templates use "address" for the http listener
+        new = re.sub(
             r'(^\s*address\s*=?\s*)(?:([0-9a-fA-F\.\:\[\]]+):)?(\d+)\s*$',
             lambda m: m.group(1) + ((m.group(2) + ":") if m.group(2) else "") + http_port,
-            text_new,
-            flags=re.M
+            new, flags=re.M
         )
-    
-        if text_new != text:
+
+        if new != text:
             with open(self.zope_conf, "w") as f:
-                f.write(text_new)
+                f.write(new)
 
-
-    
     def zeopack(self):
-        """ ZEO Pack
-        """
+        """ZEO Pack helper script points to correct server"""
         server = self.env.get("ZEO_ADDRESS", None)
         if not server:
             return
@@ -146,34 +141,38 @@ class Environment(object):
         else:
             host, port = (server, "8080")
 
+        if not os.path.exists(self.zeopack_conf):
+            return
+
         with open(self.zeopack_conf, 'r') as cfile:
             text = cfile.read()
-            text = text.replace('address = "8100"', 'address = "%s"' % server)
-            text = text.replace('host = "127.0.0.1"', 'host = "%s"' % host)
-            text = text.replace('port = "8100"', 'port = "%s"' % port)
+
+        text = re.sub(r'address\s*=\s*".*?"', 'address = "%s"' % server, text)
+        text = re.sub(r'host\s*=\s*".*?"', 'host = "%s"' % host, text)
+        text = re.sub(r'port\s*=\s*".*?"', 'port = "%s"' % port, text)
 
         with open(self.zeopack_conf, 'w') as cfile:
             cfile.write(text)
 
     def zeoserver(self):
-        """ ZEO Server
-        """
+        """ZEO Server extra options"""
         pack_keep_old = self.env.get("ZEO_PACK_KEEP_OLD", '')
         if pack_keep_old.lower() in ("false", "no", "0", "n", "f"):
-            with open(self.zeoserver_conf, 'r') as cfile:
-                text = cfile.read()
+            for conf in self.zeo_confs:
+                if not os.path.exists(conf):
+                    continue
+                with open(conf, 'r') as cfile:
+                    text = cfile.read()
                 if 'pack-keep-old' not in text:
                     text = text.replace(
                         '</filestorage>',
                         '  pack-keep-old false\n</filestorage>'
                     )
-
-            with open(self.zeoserver_conf, 'w') as cfile:
-                cfile.write(text)
+                    with open(conf, 'w') as cfile:
+                        cfile.write(text)
 
     def cors(self):
-        """ Configure CORS Policies
-        """
+        """Configure CORS Policies"""
         if not [e for e in self.env if e.startswith("CORS_")]:
             return
 
@@ -187,6 +186,7 @@ class Environment(object):
         allow_headers = self.env.get("CORS_ALLOW_HEADERS",
             "Accept,Authorization,Content-Type,X-Custom-Header")
         max_age = self.env.get("CORS_MAX_AGE", "3600")
+
         cors_conf = CORS_TEMPLACE.format(
             allow_origin=allow_origin,
             allow_methods=allow_methods,
@@ -199,37 +199,20 @@ class Environment(object):
             cfile.write(cors_conf)
 
     def buildout(self):
-        """ Buildout from environment variables
-        """
-        # Already configured
+        """Buildout from environment variables"""
         if os.path.exists(self.custom_conf):
             return
 
         findlinks = self.env.get("FIND_LINKS", "").strip().split()
-
-        eggs = self.env.get("PLONE_ADDONS",
-                            self.env.get("ADDONS", "")).strip().split()
-
-        zcml = self.env.get("PLONE_ZCML",
-                            self.env.get("ZCML", "")).strip().split()
-
-        develop = self.env.get("PLONE_DEVELOP",
-                               self.env.get("DEVELOP", "")).strip().split()
-
-        site = self.env.get("PLONE_SITE",
-                            self.env.get("SITE", "")).strip()
-
-        profiles = self.env.get("PLONE_PROFILES",
-                                self.env.get("PROFILES", "")).strip().split()
-
-        versions = self.env.get("PLONE_VERSIONS",
-                                self.env.get("VERSIONS", "")).strip().split()
-
+        eggs = self.env.get("PLONE_ADDONS", self.env.get("ADDONS", "")).strip().split()
+        zcml = self.env.get("PLONE_ZCML", self.env.get("ZCML", "")).strip().split()
+        develop = self.env.get("PLONE_DEVELOP", self.env.get("DEVELOP", "")).strip().split()
+        site = self.env.get("PLONE_SITE", self.env.get("SITE", "")).strip()
+        profiles = self.env.get("PLONE_PROFILES", self.env.get("PROFILES", "")).strip().split()
+        versions = self.env.get("PLONE_VERSIONS", self.env.get("VERSIONS", "")).strip().split()
         sources = self.env.get("SOURCES", "").strip().split(",")
-
         password = self.env.get("PASSWORD", "").strip()
 
-        # If profiles not provided. Install ADDONS :default profiles
         if not profiles:
             for egg in eggs:
                 base = egg.split("=")[0]
@@ -254,8 +237,6 @@ class Environment(object):
                 profiles="\n\t".join(profiles),
             )
 
-        # If we need to create a senaitesite and we have a zeo setup
-        # configure collective.recipe.senaitesite properly
         server = self.env.get("ZEO_ADDRESS", None)
         if server:
             buildout += ZEO_INSTANCE_TEMPLATE.format(
@@ -265,16 +246,14 @@ class Environment(object):
         with open(self.custom_conf, 'w') as cfile:
             cfile.write(buildout)
 
-    def setup(self, **kwargs):
+    def setup(self):
         self.buildout()
         self.cors()
         self.zeoclient()
         self.zeopack()
         self.zeoserver()
-        self.set_zeo_port()
+        self.set_zeo_bind_port()
         self.set_http_port()
-
-    __call__ = setup
 
 
 ZEO_TEMPLATE = """
@@ -343,8 +322,6 @@ http-fast-listen = off
 
 
 def initialize():
-    """ Configure Instance as ZEO Client
-    """
     environment = Environment()
     environment.setup()
 
